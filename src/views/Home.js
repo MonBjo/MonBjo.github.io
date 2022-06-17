066906d903ee6cb490ec1e6c3bd590f0cef23813
Monica Björk <m.bjork95@gmail.com>
2022-06-17 20:08:02 +0200

    Created fetch for weather api

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,22 +1,31 @@
-import './Error.css';
-import { useNavigate } from 'react-router-dom';
+import './Home.css';
 import Top from '../components/Top';
+import apiKey from '../apiKey';
+import Table from '../components/Table';
+
+import placeholderData from '../placeholderData.json';
 
 function Error() {
-  const navigate = useNavigate();
 
-  function navHome() {
-    navigate('/');
+  const baseURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
+  let location = "Arvika";
+  let timePeriod = "last7days"; // can also be "last30days" and "yeartodate"
+  let unit = "metric"; // can also be us to recive farenheit insted of celcius
+  const include = "&include=days%2Chours"
+
+  async function getWeather() {
+    const response = await fetch(`${baseURL}${location}/${timePeriod}?unitGroup=${unit}${include}&key=${apiKey}&contentType=json`);
+    const data = await response.json();
+    return data;
   }
 
+  const chosenHour = 12;
+
   return (
-    <article className="errorPage">
-      <Top title="Hello" subtitle="and welcome" />
-      <p>start slipsum code</p>
-      <p>
-        Look, just because I don't be givin' no man a foot massage don't make it right for Marsellus to throw Antwone into a glass motherfuckin' house, fuckin' up the way the nigger talks. Motherfucker do that shit to me, he better paralyze my ass, 'cause I'll kill the motherfucker, know what I'm sayin'?
-      </p>
-      <p>end slipsum code</p>
+    <article className="homePage">
+      <Top title={placeholderData.address} subtitle={placeholderData.days.length + " dagar tillbaka"} />
+
+      <Table hour={chosenHour} />
     </article>
   );
 }
