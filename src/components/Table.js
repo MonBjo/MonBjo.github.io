efd37279ac753012bbcd99d2bc24be7f235d81fa
Monica Björk <m.bjork95@gmail.com>
2022-06-17 20:07:14 +0200

    added table with temperatures

---
+++ src/components/Table.js
@@ -0,0 +1,28 @@
+import placeholderData from '../placeholderData.json';
+import './Table.css';
+
+function Table(props) {
+  const { hour } = props;
+  const displayData = placeholderData.days.map((day, key) => {
+    return (
+      <tr key={key}>
+        <td>{day.datetime}</td>
+        <td>{day.hours.at(hour).datetime.slice(0, -3)}</td>
+        <td>{day.hours.at(hour).temp + "°"}</td>
+      </tr>
+    )
+  });
+
+  return (
+    <table>
+        <tr>
+          <th>Datum</th>
+          <th>Tid</th>
+          <th>Tempratur</th>
+        </tr>
+        {displayData}
+    </table>
+  );
+}
+
+export default Table;
