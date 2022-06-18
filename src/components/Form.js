070ac3a94e427f1363c43b1bbc3747a973a49ef6
Monica Björk <m.bjork95@gmail.com>
2022-06-18 20:29:59 +0200

    created form

--- src/components/Form.js
+++ src/components/Form.js
@@ -23,102 +23,47 @@ function Form() {
 
   return (
     <form className="form">
-      <section className="form--section">
-        <lable className="form--title" for="location">Plats</lable>
-        <input 
-          className="form--input"
-          type="text"
-          id="location"
-          placeholder="Arvika"
-          value={ location }
-          onChange={ getLocation }
-        />
+      <section className="form__section">
+        <lable className="form__title" for="location">Plats</lable>
+        <input className="form__input" type="text" id="location" placeholder="Arvika" value={ location } onChange={ getLocation } />
       </section>
 
-      <section className="form--section">
-        <lable className="form--title">Tidsspann</lable>
-        <section className="form--radio">
-          <input 
-            className="form--input"
-            type="radio"
-            name="timespan"
-            id="week"
-            value={ timespan }
-            onChange={ getTimespan }
-          />
-          <lable className="form--option" for="week">Vecka</lable>  
+      <section className="form__section">
+        <lable className="form__title">Tidsspann</lable>
+        <section className="form__radio">
+          <input className="form__input" type="radio" name="timespan" id="week" value={ timespan } onChange={ getTimespan } />
+          <lable className="form__option" for="week">Vecka</lable>  
         </section>
         
-        <section className="form--radio">
-          <input 
-            className="form--input"
-            type="radio"
-            name="timespan"
-            id="month"
-            value={ timespan }
-            onChange={ getTimespan }
-          />
-          <lable className="form--option" for="month">Månad</lable>  
+        <section className="form__radio">
+          <input className="form__input" type="radio" name="timespan" id="month" value={ timespan } onChange={ getTimespan } />
+          <lable className="form__option" for="month">Månad</lable>  
         </section>
 
-        <section className="form--radio">
-          <input 
-            className="form--input"
-            type="radio"
-            name="timespan"
-            id="turnOfYear"
-            value={ timespan }
-            onChange={ getTimespan }
-          />
-          <lable className="form--option" for="turnOfYear">Årsskiftet</lable>  
+        <section className="form__radio">
+          <input className="form__input" type="radio" name="timespan" id="turnOfYear" value={ timespan } onChange={ getTimespan } />
+          <lable className="form__option" for="turnOfYear">Årsskiftet</lable>  
         </section>
       </section>
 
-      <section className="form--section">
-        <lable className="form--title" for="temp">Tid för tempratur</lable>
+      <section className="form__section">
+        <lable className="form__title" for="temp">Tid för tempratur</lable>
         
-        <section className="form--radio">
-          <input 
-            className="form--input"
-            type="radio"
-            name="temp"
-            id="tempHour"
-            value={ tempTime }
-            onChange={ getTempTime }
-          />
-          <input 
-            className="form--input"
-            type="number"
-            id="tempHour"
-            name="temp"
-            placeholder="13"
-            value={ tempTime }
-            onChange={ getTempTime }
-          />
+        <section className="form__radio">
+          <input className="form__input" type="radio" name="temp" id="tempHour" value={ tempTime } onChange={ getTempTime } />
+          <input className="form__input" type="number" id="tempHour" name="temp" placeholder="12" value={ tempTime } onChange={ getTempTime } />
         </section>
 
-        <section className="form--radioButton">
-          <input 
-            className="form--input"
-            type="radio"
-            name="temp"
-            id="highestTemp"
-            value={ tempTime }
-            onChange={ getTempTime }
-          />
-          <lable className="form--option" for="highestTemp">Dagens högsta tempratur</lable>  
-        </section>
+        <section className="form__radioButtonWrapper">
+          <section className="form__radioButton">
+            <input className="form__input" type="radio" name="temp" id="highestTemp" value={ tempTime } onChange={ getTempTime } />
+            <lable className="form__option" for="highestTemp">Dagens högsta tempratur</lable>  
+          </section>
 
-        <section className="form--radioButton">
-          <input 
-            className="form--input"
-            type="radio"
-            name="temp"
-            id="lowestTemp"
-            value={ tempTime }
-            onChange={ getTempTime }
-          />
-          <lable className="form--option" for="lowestTemp">Dagens lägsta tempratur</lable>
+          <section className="form__radioButton">
+            <input className="form__input" type="radio" name="temp" id="lowestTemp" value={ tempTime } onChange={ getTempTime } />
+            <lable className="form__option" for="lowestTemp">Dagens lägsta tempratur</lable>
+          </section>
         </section>
       </section>
     </form>
