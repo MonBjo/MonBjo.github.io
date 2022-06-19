781863ee5c539f3ffa22a4c5deaa7955e5c00e7a
Monica Björk <m.bjork95@gmail.com>
2022-06-19 13:31:07 +0200

    Finished form in html/jsx

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,6 @@
 import './Form.css';
 import { useState } from 'react';
+import placeholderData from '../placeholderData.json';
 
 function Form() {
   const [location, setLocation] = useState("");
@@ -20,6 +21,14 @@ function Form() {
     setTempTime(event.target.value);
     console.log(event.target);
   }
+
+
+  const displayData = placeholderData.days[0].hours.map((hours, key) => {
+    return (
+      <option>Kl {hours.datetime.slice(0, -3)}</option>
+    )
+  });
+
 
   return (
     <form className="form">
@@ -48,23 +57,11 @@ function Form() {
 
       <section className="form__section">
         <lable className="form__title" for="temp">Tid för tempratur</lable>
-        
-        <section className="form__radio">
-          <input className="form__input" type="radio" name="temp" id="tempHour" value={ tempTime } onChange={ getTempTime } />
-          <input className="form__input" type="number" id="tempHour" name="temp" placeholder="12" value={ tempTime } onChange={ getTempTime } />
-        </section>
-
-        <section className="form__radioButtonWrapper">
-          <section className="form__radioButton">
-            <input className="form__input" type="radio" name="temp" id="highestTemp" value={ tempTime } onChange={ getTempTime } />
-            <lable className="form__option" for="highestTemp">Dagens högsta tempratur</lable>  
-          </section>
-
-          <section className="form__radioButton">
-            <input className="form__input" type="radio" name="temp" id="lowestTemp" value={ tempTime } onChange={ getTempTime } />
-            <lable className="form__option" for="lowestTemp">Dagens lägsta tempratur</lable>
-          </section>
-        </section>
+        <select value={ tempTime } onChange={ getTempTime } >
+          <option value="tempmax">Dagens högsta</option>
+          <option value="tempmin">Dagens lägsta</option>
+          {displayData}
+        </select>
       </section>
     </form>
   );
